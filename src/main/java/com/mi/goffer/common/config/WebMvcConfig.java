3561d0baaff4647f96ae1443b4531b65c16b042d
TwentyfiveBTea <1248102041@qq.com>
2026-03-22 01:12:43 +0800

    feat: 添加WebMvc配置类实现拦截器和跨域配置

---
+++ src/main/java/com/mi/goffer/common/config/WebMvcConfig.java
@@ -0,0 +1,40 @@
+package com.mi.goffer.common.config;
+
+import com.mi.goffer.common.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: TwentyFiveBTea
+ * @Date: 2026/3/22 01:09
+ * @Description: 拦截器配置类
+ */
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+
+                ); // 无需任何权限
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
+
