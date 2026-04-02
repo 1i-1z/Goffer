dfcbaa1e5d7e0516956c755b78f4ccdb6aa789f1
TwentyfiveBTea <1248102041@qq.com>
2026-04-02 11:15:54 +0800

    feat(controller): ChatController 新增获取会话标题接口

--- src/main/java/com/mi/goffer/controller/ChatController.java
+++ src/main/java/com/mi/goffer/controller/ChatController.java
@@ -1,17 +1,19 @@
 package com.mi.goffer.controller;
 
 import com.mi.goffer.common.context.UserContext;
+import com.mi.goffer.common.convention.result.Result;
+import com.mi.goffer.common.convention.result.Results;
 import com.mi.goffer.dto.req.ChatReqDTO;
 import com.mi.goffer.dto.resp.ChatRespDTO;
+import com.mi.goffer.dto.resp.TitleRespDTO;
 import com.mi.goffer.service.AssistantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 /**
  * @Author: TwentyFiveBTea
@@ -26,7 +28,18 @@ public class ChatController {
     private final AssistantService assistantService;
 
     /**
+     * 获取所有会话标题
+     *
+     * @return List<TitleRespDTO> 会话标题列表
+     */
+    @GetMapping("/get-title")
+    public Result<List<TitleRespDTO>> getTitle() {
+        return Results.success(assistantService.getAllChatTitle(UserContext.getCurrentUserId()));
+    }
+
+    /**
      * 普通对话
+     *
      * @param reqDTO 请求参数
      * @return Flux<String> 流式响应
      */
